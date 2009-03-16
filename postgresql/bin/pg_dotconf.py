661e08de97e4c48d23508be29f37a00508808184
James William Pye <x@jwp.name>
2009-03-15 17:59:55 -0700

    Improve error output and --help.

--- postgresql/bin/pg_dotconf.py
+++ postgresql/bin/pg_dotconf.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import sys
+import os
 from optparse import OptionParser
-from .. import configfile as pg_cf
+from .. import configfile
 
 __all__ = ['command']
 
@@ -10,20 +11,20 @@ def command(args):
 	pg_dotconf script entry point.
 	"""
 	op = OptionParser(
-		"%prog [--stdout] [-f settings] config_file_src ([param=val]|[param])+",
-		version = '0'
+		"%prog [--stdout] [-f settings] postgresql.conf ([param=val]|[param])*",
+		version = '1.0'
 	)
 	op.add_option(
 		'-f', '--file',
 		dest = 'settings',
-		help = 'The file of settings to push; parameter arguments may override',
+		help = 'A file of settings to *apply* to the given "postgresql.conf"',
 		default = [],
 		action = 'append',
 	)
 	op.add_option(
 		'--stdout',
 		dest = 'stdout',
-		help = 'Redirect the changed file to standard output',
+		help = 'Redirect the product to standard output instead of writing back to the "postgresql.conf" file',
 		action = 'store_true',
 		default = False
 	)
@@ -35,22 +36,35 @@ def command(args):
 	for sfp in co.settings:
 		with open(sfp) as sf:
 			for line in sf:
-				pl = pg_cf.parse_line(line)
+				pl = configfile.parse_line(line)
 				if pl is not None:
 					if comment not in line[pl[0].start]:
 						settings[line[pl[0]]] = unquote(line[pl[1]])
 
+	prev = None
 	for p in ca[1:]:
 		if '=' not in p:
 			k = p
 			v = None
 		else:
 			k, v = p.split('=', 1)
+		k = k.strip()
+		if not k:
+			sys.stderr.write("ERROR: invalid setting, %r after %r%s" %(
+				p, prev, os.linesep
+			))
+			sys.stderr.write(
+				"HINT: Settings must take the form 'setting=value' " \
+				"or 'setting_name_to_comment'. Settings must also be received " \
+				"as a single argument." + os.linesep
+			)
+			sys.exit(1)
+		prev = p
 		settings[k] = v
 
 	fp = ca[0]
 	with open(fp, 'r') as fr:
-		lines = pg_cf.alter_config(settings, fr)
+		lines = configfile.alter_config(settings, fr)
 
 	if co.stdout or fp == '/dev/stdin':
 		for l in lines:
