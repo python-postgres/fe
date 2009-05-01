def482cb5171c2be1bc8b79434a7016f821f3047
James William Pye <x@jwp.name>
2009-04-30 17:33:35 -0700

    Note release date.
    Conflicts:
    
    	postgresql/documentation/changes.py

--- postgresql/documentation/changes.py
+++ postgresql/documentation/changes.py
@@ -17,8 +17,8 @@ Changes
  * Fix memory leak due to circular references and __del__. [Reported by Valentine Gogichashvili]
    Additionally, try to avoid circular references at that level.
 
-0.8.1
------
+0.8.1 released on 2009-04-30
+----------------------------
 
  * Fix memory leak due to circular references and __del__. [Reported by Valentine Gogichashvili]
  * Fix encoding normalization. [Reported by Marc Silver]
