9f5a2f119b4d5ad7a64b3b3aedc09335a8b97500
James William Pye <x@jwp.name>
2009-05-30 10:02:20 -0700

    dead code.

--- postgresql/documentation/__init__.py
+++ postgresql/documentation/__init__.py
@@ -6,14 +6,3 @@ r"""
 See: `postgresql.documentation.index`
 """
 __docformat__ = 'reStructuredText'
-
-# -m rejects this, so make the .index module the, well, index.
-if __name__ == '__main__':
-	import sys
-	if (sys.argv + [None])[1] == 'dump':
-		sys.stdout.write(__doc__)
-	else:
-		try:
-			help(__package__)
-		except NameError:
-			help(__name__)
