5c6083e4875e6d1243eaad69d2fbe20f1202a882
James William Pye <x@jwp.name>
2010-02-07 16:56:22 -0700

    Add 'description' to .project.

--- postgresql/project.py
+++ postgresql/project.py
@@ -1,7 +1,8 @@
 # project information
 name = 'py-postgresql'
 identity = 'http://python.projects.postgresql.org/'
-author = "James William Pye <x@jwp.name>"
+author = 'James William Pye <x@jwp.name>'
+description = 'Driver and tools library for PostgreSQL'
 
 # Set this to the target date when approaching a release.
 date = None
