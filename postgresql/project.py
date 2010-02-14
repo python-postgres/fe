87277bb98e42848bf74ff742e51ff17e802fee94
James William Pye <x@jwp.name>
2010-02-13 18:40:09 -0700

    Add comments and doc.

--- postgresql/project.py
+++ postgresql/project.py
@@ -1,6 +1,11 @@
-# project information
+'project information'
+
+#: project name
 name = 'py-postgresql'
+
+#: IRI based project identity
 identity = 'http://python.projects.postgresql.org/'
+
 author = 'James William Pye <x@jwp.name>'
 description = 'Driver and tools library for PostgreSQL'
 
