c738faf6bc9381fb6f8d9cc3a86ded93e20ee2a9
James William Pye <x@jwp.name>
2010-04-24 02:01:27 -0700

    Ready 1.0.1 release.

--- postgresql/project.py
+++ postgresql/project.py
@@ -10,7 +10,7 @@ author = 'James William Pye <x@jwp.name>'
 description = 'Driver and tools library for PostgreSQL'
 
 # Set this to the target date when approaching a release.
-date = None
+date = 'Sat Apr 24 01:43:11 MST 2010'
 tags = set(('fixes',))
 version_info = (1, 0, 1)
 version = '.'.join(map(str, version_info)) + (date is None and 'dev' or '')
