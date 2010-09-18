cf3540ab63272f579efe93368beee43ffa98baeb
James William Pye <x@jwp.name>
2010-09-18 12:18:45 -0700

    Prepare for v1.0.2

--- postgresql/project.py
+++ postgresql/project.py
@@ -10,7 +10,7 @@ author = 'James William Pye <x@jwp.name>'
 description = 'Driver and tools library for PostgreSQL'
 
 # Set this to the target date when approaching a release.
-date = None
-tags = set(('documentation','fixes'))
+date = 'Sat Sep 18 12:17:29 MST 2010'
+tags = set(('documentation','fixes','features','deprecations'))
 version_info = (1, 0, 2)
 version = '.'.join(map(str, version_info)) + (date is None and 'dev' or '')
