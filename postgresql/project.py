3129dcb498e9366da68efbcb1bf2699342767303
James William Pye <x@jwp.name>
2010-09-19 11:48:37 -0700

    Move v1.0 onto v1.0.3 development.

--- postgresql/project.py
+++ postgresql/project.py
@@ -10,7 +10,7 @@ author = 'James William Pye <x@jwp.name>'
 description = 'Driver and tools library for PostgreSQL'
 
 # Set this to the target date when approaching a release.
-date = 'Sat Sep 18 12:17:29 MST 2010'
-tags = set(('documentation','fixes','features','deprecations'))
-version_info = (1, 0, 2)
+date = None
+tags = set(())
+version_info = (1, 0, 3)
 version = '.'.join(map(str, version_info)) + (date is None and 'dev' or '')
