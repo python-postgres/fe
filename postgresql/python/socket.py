e4fc3db6c58d1b03c1d075f539ad16cdd6134910
James William Pye <x@jwp.name>
2011-09-14 15:13:59 -0500

    Fix a resource warning caused by a forgotten socket.
    The common situation occurred when connecting to 'localhost' where the addrinfo
    returns IPv6 addresses that aren't fully functional or in use.

--- postgresql/python/socket.py
+++ postgresql/python/socket.py
@@ -66,7 +66,7 @@ class SocketFactory(object):
 			s.settimeout(float(timeout) if timeout is not None else None)
 			s.connect(self.socket_connect)
 			s.settimeout(None)
-		except Exception:
+		except:
 			s.close()
 			raise
 		return s
