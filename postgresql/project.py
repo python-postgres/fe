de37467e8e09c1fc77c3c6d6b4063a027b2c0e40
James William Pye <james.pye@gmail.com>
2023-02-04 11:16:21 -0700

    Update patch level.

--- postgresql/project.py
+++ postgresql/project.py
@@ -8,5 +8,5 @@ identity = 'http://github.com/python-postgres/fe'
 meaculpa = 'python-postgres'
 abstract = 'Driver and tools library for PostgreSQL'
 
-version_info = (1, 3, 0)
+version_info = (1, 3, 1)
 version = '.'.join(map(str, version_info))
